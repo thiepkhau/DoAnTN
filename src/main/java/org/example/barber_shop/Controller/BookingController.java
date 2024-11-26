8a10bba9e3d54fbf3015b3325d85ce3be364d033
hieund1206 <ngodh4775@gmail.com>
2024-11-26 14:18:20 +0700

    update bookingcontroller

--- src/main/java/org/example/barber_shop/Controller/BookingController.java
+++ src/main/java/org/example/barber_shop/Controller/BookingController.java
@@ -18,31 +18,29 @@ public class BookingController {
     public ApiResponse<?> book(@RequestBody BookingRequest bookingRequest) {
         return new ApiResponse<>(
                 HttpStatus.CREATED.value(), "BOOK SUCCESS", bookingService.addBooking(bookingRequest)
-        );
-    }
+        );}
+
     @GetMapping("/customer-get-bookings")
     public ApiResponse<?> getBookings() {
         return new ApiResponse<>(
                 HttpStatus.OK.value(), "YOUR BOOKINGS", bookingService.getBookingsOfCustomers()
-        );
-    }
+        );}
+
     @GetMapping("/staff-get-bookings")
     public ApiResponse<?> getBookingsOfStaff() {
         return new ApiResponse<>(
                 HttpStatus.OK.value(), "STAFF BOOKINGS", bookingService.getBookingsOfStaff()
-        );
-    }
+        );}
+
     @GetMapping("/confirm-booking")
     public ApiResponse<?> confirmBooking(@RequestParam long booking_id){
         return new ApiResponse<>(
                 HttpStatus.OK.value(), "CONFIRM BOOKING", bookingService.confirmBooking(booking_id)
-        );
-    }
+        );}
 
     @GetMapping("/get-staff-work-schedule-in-week")
     public ApiResponse<?> getStaffWorkScheduleWeek(@RequestParam(required = false) Integer week, @RequestParam(required = false) Integer year, @RequestParam long staff_id) {
         return new ApiResponse<>(
                 HttpStatus.OK.value(), "STAFF WORK SCHEDULE",bookingService.getStaffWorkScheduleInWeek(week, year, staff_id)
-            );
-    }
+            );}
 }
